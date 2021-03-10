27e9c5489906f6b358ecacbbcaa8bedc4db12241
daichidd <dddpga1@gmail.com>
2021-03-11 03:49:27 +0900

    use __name__ == __main__

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import settings
 
 DISCORD_BOT_TOKEN = settings.DISCORD_BOT_TOKEN
 
-client = get_client()
 
-client.run(DISCORD_BOT_TOKEN)
+def main():
+    client = get_client()
+
+    client.run(DISCORD_BOT_TOKEN)
+
+
+if __name__ == '__main__':
+    main()
