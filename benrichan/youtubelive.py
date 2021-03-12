88e525938e766142a7dd9db6990522874112b036
daichidd <dddpga1@gmail.com>
2021-03-13 00:05:44 +0900

    add get chat

--- benrichan/youtubelive.py
+++ benrichan/youtubelive.py
@@ -13,12 +13,25 @@ class YoutubeLive():
             await self.message.channel.send('ボイスチャンネルに入ってから呼んでね')
             return
 
+        # for test
+        # await self.message.channel.send('ボイスチャンネルに入ったよ！')
+
         # benri-chan connect to voice channel
         await self.message.author.voice.channel.connect()
         chat_id = self.get_chat_id()
-        print('chat_id: ', chat_id)
-        # for test
-        # await self.message.channel.send('ボイスチャンネルに入ったよ！')
+
+        nextPageToken = None
+        iter_item = 90
+        slp_time = 10
+        for i in range(iter_item):
+            print('in for')
+            try:
+                print('in try')
+                nextPageToken = self.get_chat(chat_id, nextPageToken)
+                time.sleep(slp_time)
+            except Exception as e:
+                print('error ', e)
+                break
 
     def get_video_id(self, command):
         pos = command.find('=')
@@ -34,9 +47,19 @@ class YoutubeLive():
         data = requests.get(api_url, params=params).json()
 
         liveDetails = data['items'][0]['liveStreamingDetails']
+        chat_id = None
         if 'activeLiveChatId' in liveDetails.keys():
             chat_id = liveDetails['activeLiveChatId']
-        else:
-            chat_id = None
 
         return chat_id
+
+    def get_chat(self, chat_id, pageToken):
+        api_url = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
+        params = {'key': settings.YOUTUBE_API_KEY, 'liveChatId': chat_id, 'part': 'id,snippet,authorDetails'}
+
+        if type(pageToken) == str:
+            params['pageToken'] = pageToken
+
+        data = requests.get(api_url, params=params).json()
+
+        print('data', data)
